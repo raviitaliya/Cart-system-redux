e49149631c7f66649ee5f8db946f19eb281a871f
ravi italiya <italiyaravi704@gmail.com>
2024-04-29 17:43:24 +0530

    style product cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,28 +23,35 @@ const Home = () => {
         <h1 className="text-center text-xl py-3">Welcome to the store</h1>
       </div>
 
-      <div className="flex-wrap w-full">
-        {productData.map((product) => (
-          <div
-            key={product.id}
-            className="relative h-[400px] w-[300px] rounded-md"
-          >
-            <img
-              src={product.image}
-              className="z-0 h-full w-full rounded-md object-cover"
-            />
-            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-            <div className="absolute bottom-4 left-4 text-left">
-              <h1 className="text-lg font-semibold text-white">{product.title}</h1>
-              {/* <p className="mt-2 text-sm text-gray-300">
-                {product.description}
-              </p> */}
-              <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-                View Profile →
-              </button>
+      <div>
+        <h1 className="text-center text-xl pt-3 pb-10">Products</h1>
+      </div>
+
+      <div className="flex justify-center px-20 pb-32">
+        <div className="grid grid-cols-4 gap-20">
+          {productData.map((product) => (
+            <div key={product.id} className="w-[250px] rounded-md border">
+              <div className=" flex justify-center">
+                <img
+                  src={product.image}
+                  className="h-[150px] rounded-md object-cover"
+                />
+              </div>
+              <div className="p-4">
+                <h1 className="text-lg font-semibold">${product.price}</h1>
+                <p className="mt-3 h-32 text-lg text-gray-700">{product.title}</p>
+
+                
+                <button
+                  type="button"
+                  className="mt-4 w-full  rounded-sm bg-black px-2.5 py-1 text-[15px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                >
+                  Add to cart
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
