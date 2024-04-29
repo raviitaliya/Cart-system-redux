1c9eccf54e58bff742096af1f154740606a66d06
ravi italiya <italiyaravi704@gmail.com>
2024-04-29 17:50:23 +0530

    fixed navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
+    <div className="pt-20">
       <div>
         <h1 className="text-center text-xl py-3">Welcome to the store</h1>
       </div>
@@ -30,21 +30,21 @@ const Home = () => {
       <div className="flex justify-center px-20 pb-32">
         <div className="grid grid-cols-4 gap-20">
           {productData.map((product) => (
-            <div key={product.id} className="w-[250px] rounded-md border">
+            <div key={product.id} className="w-[300px] rounded-md border">
               <div className=" flex justify-center">
                 <img
                   src={product.image}
-                  className="h-[150px] rounded-md object-cover"
+                  className="h-[160px] rounded-md object-cover"
                 />
               </div>
               <div className="p-4">
                 <h1 className="text-lg font-semibold">${product.price}</h1>
-                <p className="mt-3 h-32 text-lg text-gray-700">{product.title}</p>
+                <p className="mt-3 h-28 text-lg text-gray-700">{product.title}</p>
 
                 
                 <button
                   type="button"
-                  className="mt-4 w-full  rounded-sm bg-black px-2.5 py-1 text-[15px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                  className="mt-3 w-full  rounded-sm bg-black px-2.5 py-1 text-[15px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                 >
                   Add to cart
                 </button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div className="flex w-full h-20 justify-between px-10 border-b-2 border-gray-300">
+    <div className="flex w-full h-20 justify-between px-10 border-b-2 border-gray-300 fixed bg-white">
         <div className="text-3xl flex items-center">
             Cart System
         </div>
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div className="flex items-center gap-10 text-lg">
             <Link to='/'>Home</Link>
             <Link to='/cart'>Cart</Link>
-            <span>cart item:0</span>
+            <span className="font-semibold">cart item:0</span>
         </div>
 
     </div>
